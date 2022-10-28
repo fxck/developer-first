f6696ab17f095b02c148bbdc62fedcaa922b6bf0
Ales Rechtorik <foxxcz@gmail.com>
2022-10-28 11:38:32 +0200

    initial commit

---
+++ apps/angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'developer-first-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular';
+}
