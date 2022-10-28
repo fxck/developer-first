8780db27eb6fee24545f61d3c5243498cc90a8a0
Ales Rechtorik <foxxcz@gmail.com>
2022-10-28 17:53:44 +0200

    dotnet

--- apps/dotnet/Program.cs
+++ apps/dotnet/Program.cs
@@ -1,8 +1,18 @@
-using Microsoft.AspNetCore.Builder;
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+        });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => new Response { Status = "ok" });
+
+app.UseCors();
 
 app.Run();
